16d54e65c4b3828325bd6e228c19971695eca694
Tim <timjj22@gmail.com>
2014-04-23 22:53:57 +0000

    cleaned up and added baud rate, compression, image size settings

--- pyCam/takeImagesCam2.py
+++ pyCam/takeImagesCam2.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import serial
+import copy
 
 BAUD = 38400 
 PORT = "/dev/ttyO1"      # change this to your com port!
@@ -23,6 +24,9 @@ getDataCmd = [COMMANDSEND, 0x00, 0x32, 0x0c, 0x00, 0x0A, 0x00, 0x00, 0x00, 0x00,
 getversioncommand = [COMMANDSEND, SERIALNUM, CMD_GETVERSION, COMMANDEND]
 systemresetcommand = [COMMANDSEND, SERIALNUM, CMD_RESET, COMMANDSEND]
 
+setBaudRateCmd = [0x56, 0x00, 0x31, 0x05, 0x04, 0x01, 0x00, 0x00, 0x00]
+baudrates = {115200: [0x1A,0x26], 38400: [0x2A, 0xF2]}
+
 def checkreply(r, b):
 	try:
 		r = map (ord, r)
@@ -38,47 +42,27 @@ def checkreply(r, b):
 		print e
 	return False
 
-def sendCommand(cmd, rply_hex):
-	cmd = ''.join (map (chr, cmd))
-	print "sending................"
-	print cmd
-	s.write(cmd)
-	time.sleep(1)
-	reply = s.read(100)
-	print "reply.................."
-	print reply
-	print "end of reply..........."
-	r = list(reply)
-	if (checkreply(r, rply_hex)):
+def getVersion(cmd, replyCheck):
+	rp = serialComm(cmd, 100)
+	if rp[2] == replyCheck:
 		return True
 	return False   
 
 def controlFrame(fbuf_ctrl_cmd, ctrl_code):
 	fbuf_ctrl_cmd[-1] = ctrl_code
-	print "frame controlling"
-	print fbuf_ctrl_cmd
-	cmd_b = ''.join(map(chr, fbuf_ctrl_cmd))
-	s.write(cmd_b)
-	time.sleep(1)
-	reply = s.read(5)
-	rp = map(ord, list(reply))
-	print rp
+	rp = serialComm(fbuf_ctrl_cmd, 5)
+
 	if rp[3] == 0x00:
-		print "frame control success"
 		return True
 	return False
 
 def getBufferLen(get_fbuf_cmd, fbuf_type):
-	get_fbuf_cmd.append(fbuf_type)
-	cmd_b = ''.join(map(chr, get_fbuf_cmd))
-	s.write(cmd_b)
-	time.sleep(1)
-	reply = s.read(9)
-	rp = map(ord, list(reply))
-	print rp
+	get_fbuf = get_fbuf_cmd
+	get_fbuf.append(fbuf_type)
+
+	rp = serialComm(get_fbuf_cmd, 9)
+
 	if rp[3] == 0x00:
-		print "get buffer length success"
-		print rp[len(rp)-4:]
 		return rp[len(rp)-4:] 
 	return
 
@@ -92,17 +76,10 @@ def readBuffer(*args, **kwargs):
 			read_fbuf_cmd.append(arg)
 	highbit = kwargs['highbit'] 
 	lowbit = kwargs['lowbit']
+	length = highbit << 8 | lowbit
 	
 	print "reading buffer"
-	print read_fbuf_cmd
-	cmd = ''.join(map(chr, read_fbuf_cmd))
-	s.write(cmd)
-	time.sleep(1)
-
-	length = highbit << 8 | lowbit
-	print length
-	reply = s.read(length + 10000)
-	rp = map(ord, list(reply))
+	rp = serialComm(read_fbuf_cmd, length+10000)
 
 	dataStart = [0x76, 0x00, 0x32, 0x00, 0x00, 0xFF, 0xD8]
 	dataEnd = [0xFF, 0xD9, 0x76, 0x00, 0x32, 0x00, 0x00]
@@ -111,38 +88,53 @@ def readBuffer(*args, **kwargs):
 		print "image data received"
 		return rp[5:len(rp)-5]
 	return
+
+def setBaudRate(rate):
+	setBaudRateCmd[len(setBaudRateCmd)-2:] = baudrates[rate]
+	rp = serialComm(setBaudRateCmd, 10)
 	
-######## main ################
+def setImageSize():
+	setSizeCmd = [ 0x56, 0x00, 0x31, 0x05, 0x04, 0x01, 0x00, 0x19, 0x22]
+	return serialComm(setSizeCmd, 10)
+
+def setImageCompression():
+	compCmd = [0x56, 0x00, 0x31, 0x05, 0x04, 0x01, 0x00, 0x1A, 0x26 ]
+	return serialComm(compCmd, 10)
+
+def serialComm(cmd_list, readLen):
+	cmd = ''.join(map(chr,cmd_list))
+	s.write(cmd)
+	reply = s.read(readLen)
+	rp = map(ord, list(reply))
+	return rp
 
 s = serial.Serial(PORT, baudrate=BAUD, timeout=TIMEOUT)
-#sendCommand(systemresetcommand, 0x26)
+getVersion(getversioncommand, 0x11)
 
-sendCommand(getversioncommand, 0x11)
+setImageSize()
+setImageCompression()
 
-for i in range(2):
+setBaudRate(115200)
+serial.baudrate = 115200
+
+for i in range(4):
 	# Send FBUF_CTRL command to stop current frame updating, the parameter is 0x00.
 	controlFrame(FBUF_CTRL, 0x00)
 
 	# Send GET_FBUF_LEN command to get image lengths in FBUF.
 	buff_len = getBufferLen(GET_FBUF_LEN, 0x00)
 
-	# Send READ_FBUF to read image data, the parameters are as follows in READ_FBUF command.
-	# the FBUF frame type is 0x00
-	# control mode is 0x0F
-	# starting address is 0x00
-	# the image data is the one that we get with GET_FBUF_LEN command.
-	# delay time is used to prolong the time between image data and return command,
-	#	the default value is 3000 , and can be modified. 
+	# READ_FBUF to get the image data
 	if buff_len:
 		buff = readBuffer(READ_FBUF, 0x00, 0x0A, 
 							0x00, 0x00, 0x00, 0x00, 
 							buff_len,
-							0x01,0x00, # delay
+							0x01, 0x00, # delay
 							highbit=buff_len[2],
 							lowbit=buff_len[3])
 
 		if buff:
-			with open("image" + str(i) + ".jpg", 'w') as f:
+			with open("image" + str(int(time.time())) + ".jpg", 'w') as f:
 				for i in buff:
 					f.write(chr(i))
 			print "got photo :D"
@@ -150,5 +142,5 @@ for i in range(2):
 		# After all have finished, we need to send FBUF_CTRL command to resume frame,
 		# the parameter is 0x02
 		controlFrame(FBUF_CTRL, 0x02)
-	time.sleep(1)
 
+setBaudRate(38400)
