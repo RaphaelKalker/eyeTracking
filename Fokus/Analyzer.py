a8b9e4ed00686b619491c2da4f428c3496f3c12b
AnniHe <anni.he.13@gmail.com>
2015-10-24 19:31:37 -0400

    initial implementation of fixation distance algorithm

--- Fokus/Analyzer.py
+++ Fokus/Analyzer.py
@@ -3,21 +3,24 @@ import math
 import copy
 import time as time
 import numpy as np
+import math
 
 class Analyzer:
 
-    global THRESH, MAXVAL, MIN_AREA, RED, DIFF_VALUES, DP, MIN_DIST, MAX_HOUGH_ATTEMPTS, CROSSHAIRS, PRINTDEBUG
+    global THRESH, MAXVAL, MIN_AREA, RED, GREEN, BLUE, DIFF_VALUES, DP, MIN_DIST, MAX_HOUGH_ATTEMPTS, CROSSHAIRS, PRINTDEBUG
 
     THRESH = 220 #the threshold value
     MAXVAL = 255 #the maximum value
     MIN_AREA = 30 #the min value for creating circles
-    RED = (255,255, 0)
+    RED = (0,0,255)
+    GREEN = (0,255,0)
+    BLUE = (255,0,0)
     DIFF_VALUES = 1
     DP = 10 #Dimension in circle space (lower is faster to compute)
     MIN_DIST = 20 # the minimum distance two detected circles can be from one another
     MAX_HOUGH_ATTEMPTS = 100 #define the number of attempts to find at least one circle
     CROSSHAIRS = 5
-    PRINTDEBUG = True
+    PRINTDEBUG = False 
 
     def __init__(self):
         print 'init'
@@ -25,6 +28,8 @@ class Analyzer:
     def loadImage(self, src):
 
         originalImage = cv2.imread('image/' + src)
+
+        imgWidth,imgHeight,rgb = np.shape(originalImage)
 
         if originalImage == None:
             raise NameError('Image not found!')
@@ -54,43 +59,43 @@ class Analyzer:
         minRadius = 0
         maxRadius = 40
 
-        houghCircles = cv2.HoughCircles(imageThreshold, cv2.cv.CV_HOUGH_GRADIENT, DP, MIN_DIST,
-                                   circles, param1, param2, minRadius, maxRadius)
-
-        while(houghCircles is None):
-
-            print 'Could not find circles with param2: ' + str(param2)
-
-            # if houghCircles is not None:
-            #     print 'test'
-            #     if len(houghCircles) is 1:
-            #         print 'After adjusting found more than one'
-            #         break
-
-            if (param2 is 1):
-                print 'Failed!!!!'
-                width, height, blah = originalImage.shape
-                cv2.putText(houghTransformed,"FAILED", (width/2, height/2), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255))
-                Analyzer.showImage(self, 'Hough Circle', houghTransformed, 6)
-
-                break
-
-
-
-            param2 -= 1
-            houghCircles = cv2.HoughCircles(imageThreshold, cv2.cv.CV_HOUGH_GRADIENT, DP, MIN_DIST,
-                                   circles, param1, param2, minRadius, maxRadius)
-
-            if houghCircles is not None:
-                circles = np.round(houghCircles[0, :]).astype("int")
-                for (x,y,r) in circles:
-                    cv2.circle(houghTransformed, (x,y), r, RED, 1)
-                    lineLength = 2
-                    cv2.line(houghTransformed,(x - CROSSHAIRS, y - CROSSHAIRS),(x + CROSSHAIRS, y + CROSSHAIRS),(0,0,255),1)
-                    cv2.line(houghTransformed,(x + CROSSHAIRS, y - CROSSHAIRS),(x - CROSSHAIRS, y + CROSSHAIRS),(0,0,255),1)
-
-
-                    Analyzer.showImage(self, 'Hough Circle', houghTransformed, 6)
+#        houghCircles = cv2.HoughCircles(imageThreshold, cv2.cv.CV_HOUGH_GRADIENT, DP, MIN_DIST,
+#                                   circles, param1, param2, minRadius, maxRadius)
+#
+#        while(houghCircles is None):
+#
+#            print 'Could not find circles with param2: ' + str(param2)
+#
+#            # if houghCircles is not None:
+#            #     print 'test'
+#            #     if len(houghCircles) is 1:
+#            #         print 'After adjusting found more than one'
+#            #         break
+#
+#            if (param2 is 1):
+#                print 'Failed!!!!'
+#                width, height, blah = originalImage.shape
+#                cv2.putText(houghTransformed,"FAILED", (width/2, height/2), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255))
+#                Analyzer.showImage(self, 'Hough Circle', houghTransformed, 6)
+#
+#                break
+#
+#
+#
+#            param2 -= 1
+#            houghCircles = cv2.HoughCircles(imageThreshold, cv2.cv.CV_HOUGH_GRADIENT, DP, MIN_DIST,
+#                                   circles, param1, param2, minRadius, maxRadius)
+#
+#            if houghCircles is not None:
+#                circles = np.round(houghCircles[0, :]).astype("int")
+#                for (x,y,r) in circles:
+#                    cv2.circle(houghTransformed, (x,y), r, RED, 1)
+#                    lineLength = 2
+#                    cv2.line(houghTransformed,(x - CROSSHAIRS, y - CROSSHAIRS),(x + CROSSHAIRS, y + CROSSHAIRS),(0,0,255),1)
+#                    cv2.line(houghTransformed,(x + CROSSHAIRS, y - CROSSHAIRS),(x - CROSSHAIRS, y + CROSSHAIRS),(0,0,255),1)
+#
+#
+#                    Analyzer.showImage(self, 'Hough Circle', houghTransformed, 6)
 
 
         #OLD STUFF
@@ -116,18 +121,52 @@ class Analyzer:
                 # print "Diff2: " + str(abs(1 - area/(math.pi * math.pow(radius, 2))))
 
                 center = (x + radius, y + radius)
-                cv2.line(originalImage,(x, y + radius),(x + radius*2, y + radius),(0,0,255),1)
-                cv2.line(originalImage,(x + radius, y + radius *2),(x + radius, y),(0,0,255),1)
+#                cv2.line(originalImage,(x, y + radius),(x + radius*2, y + radius),(0,0,255),1)
+#                cv2.line(originalImage,(x + radius, y + radius *2),(x + radius, y),(0,0,255),1)
+
                 cv2.circle(originalImage, center, radius, RED, 1)
+                cv2.circle(originalImage, center, 1, RED, 1)
+
+                # cropping to find ir led reflection
+                xBoundLow = max(0, x - 20)
+                xBoundHigh = min(imgWidth, x + width + 40)
+                yBoundLow = max(0, y - 20)
+                yBoundHigh = min(imgHeight, y + height + 30)
+                cv2.rectangle(originalImage, (yBoundHigh, xBoundHigh), (yBoundLow,xBoundLow), GREEN, 1)
+                imgGrayCropped = ~imageGray[xBoundLow:xBoundHigh, yBoundLow:yBoundHigh]
+                imgGrayBin = copy.deepcopy(imgGrayCropped)
+                cv2.threshold(imgGrayBin, 200, MAXVAL, cv2.THRESH_BINARY, imgGrayBin)
+                
+                Analyzer.showImage(self, 'cropped', imgGrayCropped, 5)
+                Analyzer.showImage(self, 'cropped thresholding', imgGrayBin, 6)
+
+                # get ir led reflection contour
+                irContours, hier = cv2.findContours(imgGrayBin,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+                # take the first found contour
+                if irContours:
+                    cnt = irContours[0]
+
+                    # calculate centroid of ir contour
+                    M = cv2.moments(cnt)
+                    if M['m00'] == 0:
+                        break
+                    cx = int(M['m10']/M['m00'])
+                    cy = int(M['m01']/M['m00'])
+                    
+                    # transform centroid back to original image coordinates
+                    irX = yBoundLow + cx
+                    irY = xBoundLow + cy
+                    cv2.circle(originalImage, (irX,irY), 1, RED, 1)
+
+                    # calculate distance between pupil and contour
+                    lenIrPupil = math.sqrt((irX-center[0])**2 + (irY-center[1])**2 )
+                    print lenIrPupil
 
                 Analyzer.showImage(self, 'Final Result', originalImage, 4)
 
-
         if PRINTDEBUG:
             self.printDebugInfo()
-
-
-
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
