e4fd564c1282d1ad1609b42a8fcd3c5056ad4d3e
Raphael <raphael.kalker@gmail.com>
2015-11-30 21:28:02 -0500

    Add canny detection thresholds to trackbar

--- Fokus/DebugOptions.py
+++ Fokus/DebugOptions.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import Analyzer
 import sys
+import Const
 
 WINDOW = 'Options'
 
@@ -19,10 +20,15 @@ HOUGH_MAX_RADIUS = 40
 HOUGH_MIN_DIST = 20 # the minimum distance two detected circles can be from one another
 HOUGH_MAX_ATTEMPTS = 100 #define the number of attempts to find at least one circle
 
+CANNY_LOW = '7) Canny LB'
+CANNY_HIGH = '8) Canny UP'
+
 p1 = 0
 p2 = 0
 minR = 0
 maxR = 0
+cannyLb = 0
+cannyUb = 0
 
 
 def nothing(dummyVar = None):
@@ -30,27 +36,30 @@ def nothing(dummyVar = None):
 
 def initHoughOptions(callback):
 
-    global p1, p2, minR, maxR
+    global p1, p2, minR, maxR, cannyUb, cannyLb
 
     # Create a black image, a window
     img = np.zeros((200,300,3), np.uint8)
     cv2.namedWindow(WINDOW)
     cv2.createTrackbar(PARAM1, WINDOW, 0, Analyzer.HOUGH_PARAM1, nothing)
-
     cv2.createTrackbar(MIN_RAD, WINDOW, 0, 255, nothing)
     cv2.createTrackbar(PARAM2, WINDOW, 0, Analyzer.HOUGH_MAX_PARAM2, nothing)
-
     cv2.createTrackbar(MAX_RAD, WINDOW, 0, Analyzer.HOUGH_MAX_RADIUS, nothing)
-
     cv2.createTrackbar(WINDOW_BOUND, WINDOW, 0, 100, nothing)
+    cv2.createTrackbar(CANNY_LOW, WINDOW, 0, 255, nothing)
+    cv2.createTrackbar(CANNY_HIGH, WINDOW, 0, 255, nothing)
 
     cv2.setTrackbarPos(PARAM1, WINDOW, HOUGH_PARAM1)
     cv2.setTrackbarPos(PARAM2, WINDOW, 40)
     cv2.setTrackbarPos(MIN_RAD, WINDOW, HOUGH_MIN_RADIUS)
     cv2.setTrackbarPos(MAX_RAD, WINDOW, HOUGH_MAX_RADIUS)
 
+    cv2.setTrackbarPos(CANNY_LOW, WINDOW, 35)
+    cv2.setTrackbarPos(CANNY_HIGH, WINDOW, 150)
+
     while(1):
         cv2.imshow(WINDOW,img)
+        cv2.moveWindow(WINDOW, 0, 500)
         k = cv2.waitKey(1) & 0xFF
         if k == ord('q'):
             Analyzer.close()
@@ -62,6 +71,8 @@ def initHoughOptions(callback):
         p2Temp = cv2.getTrackbarPos(PARAM2, WINDOW)
         minRTemp = cv2.getTrackbarPos(MIN_RAD, WINDOW)
         maxRTemp = cv2.getTrackbarPos(MAX_RAD, WINDOW)
+        cannyLbTemp = cv2.getTrackbarPos(CANNY_LOW, WINDOW)
+        cannyUbTemp = cv2.getTrackbarPos(CANNY_HIGH, WINDOW)
 
         updated = False
 
@@ -81,8 +92,17 @@ def initHoughOptions(callback):
             maxR = maxRTemp
             updated = True
 
+        if cannyLbTemp != cannyLb:
+            cannyLb = cannyLbTemp
+            updated = True
+
+        if cannyUbTemp != cannyUb:
+            cannyUb = cannyUbTemp
+            updated = True
+
         if updated:
-            callback(param1 = p1, param2 = p2, minRadius = minR, maxRadius = maxR)
+            # callback(param1 = p1, param2 = p2, minRadius = minR, maxRadius = maxR)
+            callback(Const.Trackbar.Canny, cannyLb = cannyLb, cannyUb = cannyUb)
 
     cv2.destroyWindow(WINDOW)
 
