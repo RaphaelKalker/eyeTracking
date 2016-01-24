6fd59d526d7ebff0abdc817beaa071132b53c8f3
Raphael <raphael.kalker@gmail.com>
2016-01-24 17:08:17 -0500

    Add training truth set with matplotlib

---
+++ Fokus/Database.py
@@ -0,0 +1,57 @@
+import os
+from tinydb import TinyDB, Query
+
+__author__ = 'Raphael'
+
+SUBPATH = 'database'
+DB = SUBPATH + '/imgDB.json'
+
+class Database(object):
+
+    def __init__(self, databasePath=None):
+
+        if databasePath is None:
+            databasePath = DB
+            if not os.path.exists(SUBPATH):
+                os.makedirs(SUBPATH)
+
+        self.db = TinyDB(databasePath)
+        self.Eyeball = Query()
+        self.getImage('')
+
+    def getImage(self, identifier):
+        print self.db.search(self.Eyeball.fileName == '1234567899.jpg')
+
+    def cycleThroughImages(self, path):
+
+        self.db.update(EXAMPLE_JSON2, self.Eyeball.fileName == '1234567899.jpg')
+        self.getImage('')
+
+        os.chdir(path)
+
+        files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.jpg')]
+
+        for image in files:
+            self.db.insert(self.constructFields(image.__str__()))
+
+    def addEyeball(self, eyeBallObj):
+
+        condition = self.Eyeball.fileName == eyeBallObj.getFileName()
+
+        if self.db.contains(condition):
+            el = self.db.get(condition)
+            self.db.update(eyeBallObj.getDict(), eids=[el.eid])
+            print 'Edited existing item with eid: ' + str(el.eid)
+
+        else:
+            retVal = self.db.insert(eyeBallObj.getDict())
+            print 'Added new item with eid: ' + str(retVal)
+
+
+
+
+
+
+
+
+
---
+++ Fokus/TruthSet.py
@@ -0,0 +1,68 @@
+import os
+import cv2
+import Database
+from EyeDict import EyeDict
+
+if True:
+    cv2.destroyAllWindows() #avoids crash
+    import matplotlib
+    matplotlib.use('TkAgg')
+    import matplotlib.pyplot as plt
+    import pylab
+    import matplotlib.image as mpimg
+
+RED = (0,0,255)
+
+class TruthSet(object):
+
+    def __init__(self, filePath):
+        pass
+
+        self.dbHelper = Database.Database()
+        self.cycle(filePath)
+
+    def onPointSelected(self, event):
+
+        if event.xdata is None or event.ydata is None:
+            #wasn't a valid click, don't close the window yet!
+            plt.waitforbuttonpress()
+            return
+
+        print ' x=%d, y=%d, xdata=%f, ydata=%f'%(
+            event.x, event.y, event.xdata, event.ydata)
+
+        eyeball = EyeDict(self.fileName)
+        eyeball.addPupilTruth(int(event.xdata), int(event.ydata))
+
+        self.dbHelper.addEyeball(eyeball)
+
+
+    def cycle(self, filePath):
+        os.chdir(filePath)
+        files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.jpeg') or f.endswith('.jpg')]
+
+        for file in files:
+            self.image = cv2.imread(file.__str__(), 0)
+            self.annotated = self.image.copy()
+            self.fileName = file.__str__()
+
+            print '\nLoading ' + self.fileName
+
+            fig = plt.figure()
+            plt.imshow(self.image, cmap='magma')
+            plt.get_current_fig_manager().window.wm_geometry("+0+0")
+            fig.canvas.mpl_connect('button_press_event', self.onPointSelected)
+            plt.waitforbuttonpress()
+            plt.close(fig)
+
+    def initMouseCallback(self, fileName):
+        cv2.namedWindow(fileName)
+        cv2.setMouseCallback(fileName, self.onPointSelected)
+        pass
+
+
+#run your stuff here
+ts = TruthSet('image/tim_jan13')
+
+
+
