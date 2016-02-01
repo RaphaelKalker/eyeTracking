b6238a2bedc04efddf6435942d5787cb34b4488f
Raphael <raphael.kalker@gmail.com>
2016-01-31 20:19:01 -0500

    Add constants and helper methods to eyeball dictionary

--- Fokus/EyeDict.py
+++ Fokus/EyeDict.py
@@ -1,12 +1,26 @@
-
+from learning.ParamsNew import ParamsNew
 
 SCHEMA = {"fileName":"","cameraType":"","color":"","verified":{"x":"","y":"","radius":""},"imageProperties":{"mean":"","variance":""},"heuristics":[{"histogramNormalized":"","threshold":"","hough":{"param1":"","param2":"","minRadius":"","maxRadius":"","x":"","y":"","radius":""},"contour":{"minRadius":"","x":"","y":"","radius":""},}]}
+
+VERIFIED = 'verified'
+X = 'x'
+Y = 'y'
+HEURISTICS = 'heuristics'
 
 class EyeDict(object):
 
     def __init__(self, fileName = None):
         self.dict = SCHEMA
         self.dict['fileName'] = fileName
+
+
+    def useParams(self, params):
+        if isinstance(params, ParamsNew):
+            print 'build eye from params'
+            pass
+        else:
+            print 'Parameters instance is incorrect.'
+            # raise ValueError('Parameters instance is incorrect.')
 
     def getDict(self):
         return self.dict
@@ -15,6 +29,9 @@ class EyeDict(object):
         return self.dict['fileName']
 
     def addPupilTruth(self, x, y):
-        self.dict['verified']['x'] = x
-        self.dict['verified']['y'] = y
+        self.dict[VERIFIED][X] = x
+        self.dict[VERIFIED][Y] = y
         pass
+
+    def addThreshold(self, min, max, isNormalized):
+        self.dict[HEURISTICS]
