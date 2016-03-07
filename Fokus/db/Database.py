e44b99324a16a4ea28098c0d2204594e5642296f
AnniHe <anni.he.13@gmail.com>
2016-03-06 20:59:53 -0500

    changed database static functions to class functions

--- Fokus/db/Database.py
+++ Fokus/db/Database.py
@@ -6,55 +6,53 @@ if Utils.isMac():
 __author__ = 'Raphael'
 
 SUBPATH = 'database'
-DB = SUBPATH + '/imgDB-2.json'
-
-global helper
-
-databasePath = DB
-if not os.path.exists(SUBPATH):
-    os.makedirs(SUBPATH)
-
-helper = TinyDB(databasePath)
-
-
-def getImage(identifier):
-    return helper.get(Query().fileName == identifier)
+#DB = SUBPATH + '/imgDB-2.json'
+#
+#global helper
+#
+#databasePath = DB
+#if not os.path.exists(SUBPATH):
+#    os.makedirs(SUBPATH)
+#
+#helper = TinyDB(databasePath)
 
 
-def getTruth(identifier):
-    entry = getImage(identifier)
-
-    x = entry['truth']['x']
-    y = entry['truth']['y']
-
-    # some times the value is stored as a unicode string, we need an int
-    if isinstance(x, basestring):
-        x = int(x)
-        y = int(y)
-
-    validEntry = False if entry is None or x == -1 or y == -1 else True
-
-    # if (entry is None or
-    #     x == -1 or
-    #     y == -1):
-    #     return False, (x, x)
-
-
-    return validEntry, (x, y)
+#def getImage(identifier):
+#    return helper.get(Query().fileName == identifier)
+#
+#
+#def getTruth(identifier):
+#    entry = getImage(identifier)
+#
+#    x = entry['truth']['x']
+#    y = entry['truth']['y']
+#
+#    # some times the value is stored as a unicode string, we need an int
+#    if isinstance(x, basestring):
+#        x = int(x)
+#        y = int(y)
+#
+#    validEntry = False if entry is None or x == -1 or y == -1 else True
+#
+#    # if (entry is None or
+#    #     x == -1 or
+#    #     y == -1):
+#    #     return False, (x, x)
+#
+#
+#    return validEntry, (x, y)
 
 
 class Database(object):
     def __init__(self, databaseName=None):
 
-        # if databasePath is None:
-        #     databasePath = DB
-        #     if not os.path.exists(SUBPATH):
-        #         os.makedirs(SUBPATH)
-        # else:
-        databasePath = SUBPATH + '/' + databaseName + '.json'
+        if os.path.exists(databaseName):
+            databasePath = databaseName
+        else:
+            databasePath = SUBPATH + '/' + databaseName + '.json'
 
-        if not os.path.exists(SUBPATH):
-            os.makedirs(SUBPATH)
+            if not os.path.exists(SUBPATH):
+                os.makedirs(SUBPATH)
 
         self.db = TinyDB(databasePath)
         self.Eyeball = Query()
@@ -78,12 +76,26 @@ class Database(object):
     def eyeBallExists(self, fileName):
         return self.db.contains(self.Eyeball.fileName == fileName)
 
-#
-#
-#
-#
-#
-#
-#
-#
-#
+    def getTruth(self, identifier):
+        entry = self.getImage(identifier)
+
+        x = entry[0]['truth']['x']
+        y = entry[0]['truth']['y']
+
+        # some times the value is stored as a unicode string, we need an int
+        if isinstance(x, basestring):
+            x = int(x)
+            y = int(y)
+
+        validEntry = False if entry is None or x == -1 or y == -1 else True
+
+        # if (entry is None or
+        #     x == -1 or
+        #     y == -1):
+        #     return False, (x, x)
+
+
+        return validEntry, (x, y)
+    
+    def getSearchFileMatch(self, query):
+        return self.db.search(self.Eyeball.fileName.matches(query))
