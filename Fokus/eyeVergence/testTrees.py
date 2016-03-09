84de4ede55febea2c3eb590bf350115530acdbc9
AnniHe <anni.he.13@gmail.com>
2016-03-08 22:37:47 -0500

    decision tree implementation in python

--- Fokus/eyeVergence/testTrees.py
+++ Fokus/eyeVergence/testTrees.py
@@ -1,4 +1,16 @@
-
+import sys
+import csv
 from BinaryTree import DecisionTree
+
 a = DecisionTree()
-t = a.importTree('./trees/tree1.csv')
+a.importTree('./trees/tree1.csv')
+
+with open(sys.argv[1], 'r') as f: 
+    csvReader = csv.reader(f, delimiter=',')
+    fields = next(csvReader)
+
+    for r in csvReader:
+        x = [float(x) for x in r]
+        test = {'x1':x[0], 'x2': x[1], 'x3': x[2],'x4':x[3]}
+        ret = a.traverseTree(test, a.root)
+        print ret
