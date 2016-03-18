386b516dde1f7be058a9e2555f275ca7cc8f3cc8
Raphael <raphael+kalker+gitlab@gmail.com>
2016-03-18 18:53:15 -0400

    Add more debug flags

--- Fokus/debug/FeatureDebug.py
+++ Fokus/debug/FeatureDebug.py
@@ -2,7 +2,7 @@ import Utils
 
 __author__ = 'Raphael'
 
-SHOW_CV2_IMAGES = True
+SHOW_CV2_IMAGES = False
 
 SAVE_IMAGES = False
 TRACKBAR = False
@@ -15,9 +15,10 @@ NORMALIZE_GRAYSCALE = True
 START_TRUTH_FROM_PREV = False
 VERIFY_TRUTH = True
 BLOB_DETECTOR = True if Utils.isMac() else False
+COMPARE_WITH_MATPLOTLIB = True
 
-#DEBUG WINDOW
 DEBUG_PUPIL_DETECTOR = False if Utils.isMac() else False
+DEBUG_BLOB_DETECTOR = True if Utils.isMac() else False
 
 #PRINT DEBUG
 PRINT_HEURISTICS = False
