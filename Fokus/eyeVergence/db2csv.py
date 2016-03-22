e366336bb2ae3db53076a15d452dcf5abf8ce2f8
Timothy Jeruzalski <timjj22@gmail.com>
2016-03-21 21:17:19 -0400

    Lamp dataset additions and small fix in the db2csv

--- Fokus/eyeVergence/db2csv.py
+++ Fokus/eyeVergence/db2csv.py
@@ -2,9 +2,20 @@ import sys
 import csv
 from db import Database
 
-def getPupils(dh, timestamp):
+#USAGE: python db2csv.py <DB1 path> <DB2 path> <csv name>
+
+def getPupilsDISTANCE(dh, timestamp):
     leftImg = "L" + timestamp
     rightImg = "R" + timestamp
+    
+    l_valid, l_pupil = dh.getTruth(leftImg)
+    r_valid, r_pupil = dh.getTruth(rightImg)
+    if l_valid and r_valid:
+        return (l_pupil, r_pupil)
+
+def getPupilsREADING(dh, timestamp):
+    leftImg = "R" + timestamp
+    rightImg = "L" + timestamp
     
     l_valid, l_pupil = dh.getTruth(leftImg)
     r_valid, r_pupil = dh.getTruth(rightImg)
@@ -18,14 +29,14 @@ with open(sys.argv[3], 'w') as f:
     db_reading = Database.Database(sys.argv[1])
     r_list = db_reading.getSearchFileMatch('^L')
     for frame in r_list:
-        ret = getPupils(db_reading, frame['fileName'][1:])
+        ret = getPupilsREADING(db_reading, frame['fileName'][1:])
         if ret:
             writer.writerow( (ret[0][0], ret[0][1], ret[1][0], ret[1][1], 0) )
 
     db_distance = Database.Database(sys.argv[2])
     d_list = db_distance.getSearchFileMatch('^L')
     for frame in d_list:
-        ret = getPupils(db_distance, frame['fileName'][1:])
+        ret = getPupilsDISTANCE(db_distance, frame['fileName'][1:])
         if ret:
             writer.writerow( (ret[0][0], ret[0][1], ret[1][0], ret[1][1], 1) )
     
